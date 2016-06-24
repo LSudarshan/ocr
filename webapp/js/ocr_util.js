dfd2a510c8c121dccf3fbc976e9afff1774fbac9
suraj <surajmbabar@gmail.com>
2016-06-24 08:58:45 +0530

    Refactoring ocr_utils, extracting functions

--- webapp/js/ocr_util.js
+++ webapp/js/ocr_util.js
@@ -7,20 +7,31 @@ var set_ocr_data = function(ocr_data){
 	ocr_numbers = ocr_data.numbers;
 }
 
-var findFirstNumberOnTheRightOfTheWord = function(coordinates){
-  return ocr_numbers.filter(function(ocr_number){
-    return (ocr_number.coordinates.y1 == coordinates.y1 ||
-    ocr_number.coordinates.y2 == coordinates.y2)
-  }).sort(
-    function(a,b){
-      a.coordinates.x1 < b.coordinates.x1
-    })[0];
+var inAscendingOrderByXCoordinate = function(first_number,second_number){
+  return first_number.coordinates.x1 >= second_number.coordinates.x1
+}
+
+var isCoordinateSame = function(coordinate, word1, word2){
+	return word1.coordinates[coordinate] == word2.coordinates[coordinate];
+}
+var areBothOnSameLine = function(word1, word2){
+	return (isCoordinateSame('y1', word1, word2) ||
+			isCoordinateSame('y2', word1, word2))
+}
+
+var findNumbersInTheSameLine = function(word){
+	return ocr_numbers.filter(function(ocr_number){
+      return areBothOnSameLine(word,ocr_number);
+  })
+}
+var findFirstNumberOnTheRightOfTheWord = function(word){
+  return findNumbersInTheSameLine(word).sort(inAscendingOrderByXCoordinate)[0];
 }
 
 var findAssociatedNumbers = function(word){
   var ocr_word_data = checkForAllSynonyms(word);
   if(ocr_word_data.length == 0) return;
-  return findFirstNumberOnTheRightOfTheWord(ocr_word_data[0].coordinates)
+  return findFirstNumberOnTheRightOfTheWord(ocr_word_data[0])
 }
 
 var checkForAllSynonyms = function(word){
